dc81578cff4c7a1db82dd1c7e2cc2ed2c89ecde9
cbh778899 <cbh778899@outlook.com>
2024-09-02 18:50:40 +1000

    update basic home page styles
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ src/utils/types.js
@@ -0,0 +1,18 @@
+export const projects = {
+    Voyager: { 
+        description: "Voyager is a project written by Node.JS, to build OpenAI-like APIs on users' local machine. It comes with a powerful CLI and easy-setup commands and docker containers for users to quickly start their own AI server." ,
+        badgers: [
+            "https://github.com/SkywardAI/voyager/actions/workflows/linter-builder-checker.yml/badge.svg",
+            "https://github.com/SkywardAI/voyager/actions/workflows/release-drafter.yml/badge.svg",
+            "https://github.com/SkywardAI/voyager/actions/workflows/release-image.yml/badge.svg"
+        ],
+        urls: [
+            {name: "GitHub", url: "https://github.com/SkywardAI/voyager"},
+            {name: "K8SUG Official Website", url: "https://k8sug.com"}
+        ],
+        video: "https://github.com/user-attachments/assets/fa7059c2-309e-486a-a28a-45867613c84b"
+    },
+    Kirin: {  },
+    Rebel: {  },
+    Shibuya: {  },
+}
