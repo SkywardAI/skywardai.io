ac3fb0e7433797d3f92d852b3bd0399bec56273c
cbh778899 <cbh778899@outlook.com>
2024-09-05 22:46:40 +1000

    adjust config to enable SharedArrayBuffer
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "skywardai.io"
+  plugins: [
+    react(),
+    // to enable SharedArrayBuffer
+    {
+      name: 'isolation',
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          next();
+        });
+      },
+    },
+  ],
+  base: "/skywardai.io"
 })
